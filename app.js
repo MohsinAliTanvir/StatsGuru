363c8b39e89e1739bd8b0854ece308981bf04706
MohsinAliTanvir <63919246+MohsinAliTanvir@users.noreply.github.com>
2021-06-18 16:43:49 +0500

    Separate functions for each stats operation

--- app.js
+++ app.js
@@ -3,86 +3,84 @@ document.getElementById('import').onclick=function(){
 
     var files=document.getElementById('selectFiles').files;
     var fr = new FileReader();
+    fr.readAsText(files.item(0));    
 
     fr.onload=function(e){
         const result=JSON.parse(e.target.result);
-        var formatted=JSON.stringify(result);
-      
-
-       // Fetching the data from the file
-       var arr= result.data;
-       var length=arr.length;
-
-       // IMPLEMENTING MEAN FUNCTIONALITY
-       var total=0; 
-       for(var i=0;i<length;i++){
-           total=total+arr[i];
-       }
-
-       var mean = total/length;
-       document.getElementById('mean').textContent = mean;
+        findMean(result);
+        findMedian(result);
+        findMode(result);
+        findStandardDeviation(result);
+    } 
+}
 
 
-       //IMPLEMENTING MEDIAN FUNCTIONALITY
-       var median=0;
-       var sorted=arr.sort();
+// IMPLEMENTING MEAN FUNCTIONALITY
+function findMean(inputData){
+    var arr= inputData.data;
+    const length=arr.length;
 
-       if(length%2==0){
-        median=(sorted[length/2] + sorted[(length/2)-1])/2;
-       }
-       else{
-           median=sorted[(length-1)/2];
-       }
+    const total= arr.reduce((acc,val) => acc+val);
+    const mean = total/length;
+    document.getElementById('mean').textContent = mean;
+    return mean;
+}
 
-       document.getElementById('median').textContent = median;
+//IMPLEMENTING MEDIAN FUNCTIONALITY
+function findMedian(inputData){
+    var arr= inputData.data;
+    var length=arr.length;
 
+    let median=0;
+    let sorted=arr.sort();
+    length%2===0 ? median=(sorted[length/2] + sorted[(length/2)-1])/2 : median=sorted[(length-1)/2];
+    document.getElementById('median').textContent = median;
+}
 
-       //IMPLEMETING THE MODE FUNCTIONALITY
-       var mode=0;
-       var count=1;
-       var value=0, prev_val=0;
-       var prev_count=0;
+//IMPLEMETING THE MODE FUNCTIONALITY
+function findMode(inputData){
+    var arr= inputData.data;
+    var length=arr.length;
 
-       for(var i=0;i<length;i++){
-        if(sorted[i]==sorted[i+1]){
-            count++;
-            value=sorted[i];
-        }
-        else{
-            if(count>prev_count){
-                prev_count=count;
-                prev_val=value;
-            }
-            count=1;
-        }
-        }
-        
-        if(count>prev_count){
-            mode=value;
-        }
-        else{mode=prev_val;
-        }
-        console.log(mode)
-        document.getElementById('mode').textContent = mode;
+    let mode=0;
+    let count=1;
+    let value=0, prev_val=0;
+    let prev_count=0;
+    let sorted=arr.sort();
 
+    for(let i=0;i<length;i++){
+     if(sorted[i]==sorted[i+1]){
+         count++;
+         value=sorted[i];
+     }
+     else{
+         if(count>prev_count){
+             prev_count=count;
+             prev_val=value;
+         }
+         count=1;
+     }
+     }
+     count>prev_count ? mode=value : mode=prev_val;
+     document.getElementById('mode').textContent = mode;
+}
 
+//IMPLEMETING THE S.D FUNCTIONALITY
+function findStandardDeviation(inputData){
+    var arr= inputData.data;
+    var length=arr.length;
 
-        //IMPLEMENTING THE S.D
-        //S.D= (MEAN OF THE SQUARED VALUES - SQAURE OF THE MEAN)^1/2
-
-        const squared_arr =[];
+    const squared_arr =[];
         var sum=0;
-        for(var i=0;i<length;i++){
+        for(let i=0;i<length;i++){
             squared_arr[i]=(arr[i]) ** 2;
             sum=sum+squared_arr[i];
         }
 
-        var mean_of_squared=sum/length;
-
-        var StandardDeviation= (mean_of_squared-(mean)**2)**(1/2);
+        const mean_of_squared=sum/length;
+        let mean=findMean(inputData);
+        const StandardDeviation= (mean_of_squared-(mean)**2)**(1/2);
         document.getElementById('standardDeviation').textContent = StandardDeviation;
-        
 
-    } 
-    fr.readAsText(files.item(0));    
+
 }
